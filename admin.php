a038c54f1fbfa4bb117c37b9b4bfd2a780506e17
Marcos Pinardi <mpinardi@digitalhouse.com>
2020-01-22 11:14:39 -0300

    setup inicial

---
+++ admin.php
@@ -0,0 +1,23 @@
+<?php  include 'includes/header.html';  ?>
+<?php  include 'includes/nav.php';  ?>
+
+<main class="container">
+    <h1>Panel de Administración</h1>
+
+    <div class="list-group">
+        <a class="list-group-item" href="adminMarcas.php">
+            Panel de administración de Marcas
+        </a>
+        <a class="list-group-item" href="adminCategorias.php">
+            Panel de administración de Categorías
+        </a>
+        <a class="list-group-item" href="adminProductos.php">
+            Panel de administración de Productos
+        </a>
+        <a class="list-group-item" href="adminUsuarios.php">
+            Panel de administración de Usuarios
+        </a>
+    </div>
+</main>
+
+<?php  include 'includes/footer.php';  ?>
